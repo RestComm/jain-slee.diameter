2ab29e6996111b10fb89d35de8b85492b70ad686
Bartosz Krok <bartosz.krok@pro-ids.com>
2017-07-06 13:48:46 +0200

    license header

--- resources/diameter-cca/common/events/src/main/java/net/java/slee/resource/diameter/cca/events/RequestTxTimeout.java
+++ resources/diameter-cca/common/events/src/main/java/net/java/slee/resource/diameter/cca/events/RequestTxTimeout.java
@@ -1,23 +1,20 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2011, Red Hat, Inc. and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2016, Telestax Inc and individual contributors
+ * by the @authors tag.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
  * the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
 package net.java.slee.resource.diameter.cca.events;
@@ -28,12 +25,12 @@ import org.jdiameter.api.Message;
 
 /**
  * Tx timer as specified in RFC 4006:
- * 
+ *
  * <pre>
  * 13.  Credit-Control Application Related Parameters
- *   
+ *
  *  Tx timer
- *   
+ *
  *   When real-time credit-control is required, the credit-control
  *   client contacts the credit-control server before and while the
  *   service is provided to an end user. Due to the real-time nature
@@ -51,21 +48,21 @@ public interface RequestTxTimeout {
 
   /**
    * Returns the request that caused Tx timeout to expire due to no response from a remote peer.
-   * 
+   *
    * @return origin request
    */
   Message getRequest();
-  
+
   /**
    * Returns the name of the last selected remote peer that a given message has been routed to.
-   * 
+   *
    * @return peer name
    */
   DiameterIdentity getLastPeer();
-  
+
   /**
    * Returns true if last selected remote peer is present for a given message.
-   * 
+   *
    * @return true if peer name is present
    */
   boolean hasLastPeer();
